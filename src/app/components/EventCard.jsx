b0cad1748472fd447f1bea56d15d6aff4277d1dc
MitchLloyd <mitchelllloyd154@gmail.com>
2024-07-22 15:01:24 +0000

    feat: render static list of ticketmaster events

---
+++ src/app/components/EventCard.jsx
@@ -0,0 +1,27 @@
+import styles from './event-card.module.css';
+
+
+// event name
+// location: city, venue
+// date: month, date, day, time
+
+import {getDay, getDate, getMonth} from 'date-fns';
+
+export default function EventCard({name, city, venue, date: dateString, time}) {
+
+    const date = getDate(new Date(dateString));
+    const dayIndex = getDay(new Date(dateString));
+    const monthIndex = getMonth(new Date(dateString));
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+    const daysInWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    return <li className={styles['event-card']}>
+            <section><p>{months[monthIndex]}</p><p>{date}</p></section>
+            <section>
+                <p>{daysInWeek[dayIndex]} - {time}</p>
+                <p>{name}</p>
+                <p>{city}</p>
+                <p>{venue}</p>
+            </section>
+        </li>
+}
---
+++ src/app/search/page.jsx
@@ -0,0 +1,5 @@
+export default async function SearchPage() {
+
+    return <section>some info here...</section>
+  
+  }
